b210a1d7f824b71511b7be16e0a24c7de86db99d
guidofenix <guido.barbaglia@fao.org>
2014-09-16 16:34:49 +0200

    Fixes for the distribution.

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from setuptools import find_packages
 
 setup(
     name='PGeoMODIS',
-    version='0.1.1',
+    version='0.1.4',
     author='Simone Murzilli; Guido Barbaglia',
     author_email='geobrickspy@gmail.com',
     packages=find_packages(),
     install_requires=[
-        'pgeo',
         'flask'
     ],
     url='http://pypi.python.org/pypi/PGeoMODIS/',
